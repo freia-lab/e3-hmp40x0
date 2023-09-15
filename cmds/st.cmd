0fc7df6e824bf269b5d3fb9f1b5bc26a10769a21
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2023-09-15 09:35:33 +0200

    Increased ReplyTimeout from default 1000 to 3000 ms.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,4 +2,8 @@
 
 require hmp40x0
 
+# Limit the error messages form the StreamDevice (specially when the device is not conn
+ected)
+var streamErrorDeadTime 60
+
 iocshLoad("$(hmp40x0_DIR)hmp4040.iocsh", "IP=192.168.10.45,P=CstatV-AC:,IOCNAME=ioc24-hmp")
